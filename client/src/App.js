312b77eaab6c9ad06a84fb393f9142b4af84f611
Matthew Morford <matthewdavidmorford@gmail.com>
2018-05-01 21:29:35 -0400

    basic router and recipes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import './App.css';
 
 import Basic from "./components/Basic/basic";
 import ModalExample from "./components/Modal";
+import Recipe from "./components/Recipe/SearchResultContainer"
 
 
-class App extends Component {
+const App = () => (
 
-  // state = {
-  //   events: []
-  // }
-    
-  // componentDidMount() {
-  //     fetch('/events')
-  //       .then(res => res.json())
-  //       .then(events => this.setState({ events: events}));
-  //   } 
+  <Router>
+    <div className="App">
+      
+      <Switch>
+        <Route exact path="/" component={Basic} />
+        <Route exact path="/events" component={Basic} />
+        <Route exact path="/recipes" component={Recipe} />
+        <Route component={Basic} />
+      </Switch>
+    </div>
+  </Router>
 
-  render() {
-    return (
-      <div className="App">
-        <Basic />
-        <ModalExample />
-      </div>
-    );
-  }
-}
+)
+
+// state = {
+//   events: []
+// }
+
+// componentDidMount() {
+//     fetch('/events')
+//       .then(res => res.json())
+//       .then(events => this.setState({ events: events}));
+//   } 
+
+// render() {
+//   return (
+//     <div className="App">
+//       <Basic />
+//       <ModalExample />
+//     </div>
+//   );
+// }
+
 
 export default App;
