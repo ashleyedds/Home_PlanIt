e1d2b47d07b09a9d7d4c710fe7d77916ede175fc
Matthew Morford <matthewdavidmorford@gmail.com>
2018-05-11 19:53:00 -0400

    Recipes saving

--- client/src/components/Recipe/ResultList.js
+++ client/src/components/Recipe/ResultList.js
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Container, Row, Col } from 'reactstrap';
 import { Nav, NavItem, NavLink } from 'reactstrap';
 import { Jumbotron, Button } from 'reactstrap';
 import {
-	Card, CardImg, CardText, CardBody,
-	CardTitle, CardSubtitle, CardGroup
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, CardGroup
 } from 'reactstrap';
 import styled, { css } from 'styled-components';
+import SaveBtn from './../SaveBtn';
+
+
 
 const ResultList = props => (
-  <div className="list-group">
-    <Row>
-      {props.results.map(result => (
-          <Col sm="4">
-            <Card>
-              <CardImg top width="100%" className="img-fluid" src={result.recipe.image} alt={result.title} />
-              <CardBody>
-                <CardTitle><h2>{result.recipe.label}</h2></CardTitle>
-                <CardText>{result.recipe.ingredients.map(ingredient => (<p>{ingredient.text}</p>))}</CardText>
-                <Button><a href={result.recipe.url}>Go to Recipe</a></Button>
-              </CardBody>
-            </Card>
-          </Col>
-      ))}
-    </Row>
-  </div>
-);
+
+ 
+      <div className="list-group">
+        <Row>
+          {props.results.map(result => (
+            <Col sm="4">
+              <Card>
+                <CardImg top width="100%" className="img-fluid" src={result.recipe.image} alt={result.title} />
+                <CardBody>
+                  <CardTitle><h2>{result.recipe.label}</h2></CardTitle>
+                  <CardText>{result.recipe.ingredients.map(ingredient => (<p>{ingredient.text}</p>))}</CardText>
+                  <Button><a href={result.recipe.url}>Go to Recipe</a></Button>
+                  <SaveBtn onClick={() => props.handleRecipeSave(result.recipe.url, result.recipe.label)} />
+                </CardBody>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+)
+
+
 
 export default ResultList;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const calendarRoutes = require("./events");
+const recipeRoutes =require("./recipe");
 
 router.use("/events", calendarRoutes);
+router.use("/recipes", recipeRoutes)
 
 module.exports = router;
