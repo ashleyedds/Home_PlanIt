4bc9ad815082b8aabe8e862a5a816197f025844a
Ashley Edds <ashley@edds.net>
2018-05-12 18:05:13 -0400

    update and delete events working

---
+++ client/src/components/DeleteBtn/DeleteBtn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "reactstrap"
+import "./DeleteBtn.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+
+const DeleteBtn = props => (
+  <Button color="danger" {...props}>Delete</Button>
+);
+
+export default DeleteBtn;
--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import BigCalendar from 'react-big-calendar';
+import DeleteBtn from "../../components/DeleteBtn";
+import UpdateBtn from "../../components/UpdateBtn";
 import EventModal from "./EventModal";
 import ModalExample from "./EditEvent"
 import moment from "moment";
 import API from "../../utils/eventAPI";
 import "./Events.css";
-import { Container, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import { Container, Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, FormText } from 'reactstrap'
+import {Input} from "./Input";
 import styled, { css } from 'styled-components';
 
 BigCalendar.momentLocalizer(moment);
@@ -17,13 +20,22 @@ class Basic extends React.Component {
         super(props);
         this.state = {
             modal: false,
+            nestedModal: false,
+            closeAll: false,
             events: [],
             title: "",
             description: "",
             starts: "",
-            ends: ""
+            startDate: "",
+            startTime: "",
+            endDate: "",
+            endTime: "",
+            ends: "",
+            id: ""
         };
         this.toggle = this.toggle.bind(this);
+        this.toggleNested = this.toggleNested.bind(this);
+        this.toggleAll = this.toggleAll.bind(this);
         }
 
         toggle() {
@@ -32,10 +44,43 @@ class Basic extends React.Component {
             });
             }
 
+        toggleNested() {
+            this.setState({
+                nestedModal: !this.state.nestedModal,
+                closeAll: false
+            });
+            }
+            
+            toggleAll() {
+            this.setState({
+                nestedModal: !this.state.nestedModal,
+                closeAll: true
+            });
+            }
+
     componentDidMount() {
 
         this.searchDb();
         
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        API.saveEvent({
+            title: this.state.title,
+            start: this.state.startDate + " " + this.state.startTime,
+            end: this.state.endDate + " " + this.state.endTime,
+            description: this.state.description
+        })
+            .catch(err => console.log(err));
+            
     }
 
     searchDb = () => {
@@ -52,13 +97,42 @@ class Basic extends React.Component {
 
     updateModal = (id) => {
         API.getEvent(id)
-        .then(res => this.setState({
-            title: res.data.title,
-            description: res.data.description,
-            starts: res.data.start,
-            ends: res.data.end
-        }))
+        .then(res => 
+            {
+            var makeStart = res.data.start.split(" ");
+            var makeEnd = res.data.end.split(" ");
+            this.setState({
+                title: res.data.title,
+                description: res.data.description,
+                starts: res.data.start,
+                ends: res.data.end,
+                startDate: makeStart[0],
+                startTime: makeStart[1],
+                endDate: makeEnd[0],
+                endTime: makeEnd[1],
+                id: res.data._id
+        })
+    })
         .then(this.toggle())
+    };
+
+    deleteEvent = id => {
+        console.log(this.state.id)
+        API.deleteEvent(id)
+            .then(console.log("success"))
+            .catch(err => console.log(err));
+        };
+    
+    editEvent = (id, event) => {
+        var updatedEvent = {
+            title: this.state.title,
+            start: this.state.startDate + " " + this.state.startTime,
+            end: this.state.endDate + " " + this.state.endTime,
+            description: this.state.description
+        }    
+        API.updateEvent(id, updatedEvent)
+        .then(console.log("success update"))
+        .catch(err => console.log(err))
     }
 
     render() {
@@ -90,11 +164,66 @@ class Basic extends React.Component {
                 <h3>{this.state.description}</h3>
                 <p>Starts at: {this.state.starts}</p>
                 <p>Ends at: {this.state.ends}</p>
+                <Modal isOpen={this.state.nestedModal} toggle={this.toggleNested} onClosed={this.state.closeAll ? this.toggle : undefined}>
+                <ModalHeader>Edit event</ModalHeader>
+                <ModalBody>
+                <Form>
+                    <FormGroup>
+                        <Label for="title">Event Title</Label>
+                        <Input 
+                            value={this.state.title}
+                            onChange={this.handleInputChange}
+                            name="title"
+                            placeholder="What are you up to?" />
+                    </FormGroup> 
+                    <FormGroup>
+                        <Label for="description">Event Description (optional)</Label>
+                        <Input 
+                            value={this.state.description}
+                            onChange={this.handleInputChange}
+                            name="description" 
+                            placeholder="Tell me more." />
+                    </FormGroup> 
+                    <FormGroup>
+                        <Label for="date">Start Date (YYYY-MM-DD)</Label>
+                        <Input
+                            value={this.state.startDate}
+                            onChange={this.handleInputChange} 
+                            name="startDate" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="startTime">Start Time</Label>
+                        <Input
+                            value={this.state.startTime}
+                            onChange={this.handleInputChange} 
+                            name="startTime" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="date">End Date (YYYY-MM-DD)</Label>
+                        <Input 
+                            value={this.state.endDate}
+                            onChange={this.handleInputChange}
+                            name="endDate" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="endTime">End Time</Label>
+                        <Input 
+                            value={this.state.endTime}
+                            onChange={this.handleInputChange}
+                            name="endTime" />
+                    </FormGroup>
+            </Form>
+                </ModalBody>
+                <ModalFooter>
+                <UpdateBtn onClick={() => this.editEvent(this.state.id)} />{' '}
+                    <Button color="secondary" onClick={this.toggleAll}>Nevermind</Button>
+                </ModalFooter>
+                </Modal>
             </ModalBody>
             <ModalFooter>
             <Button color="success" onClick={this.toggle}>Okay</Button>{' '}
-            <Button color="info" onClick={this.toggle}>Edit Event</Button>{' '}
-            <Button color="danger" onClick={this.toggle}>Delete Event</Button>
+            <Button color="info" onClick={this.toggleNested}>Edit</Button>{' '}
+            <DeleteBtn onClick={() => this.deleteEvent(this.state.id)} />
             </ModalFooter>
         </Modal>
 
