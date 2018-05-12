54b462889f86b828ce53a36fa151401f4f850e40
Matthew Morford <matthewdavidmorford@gmail.com>
2018-05-12 09:45:04 -0400

    Retrieve saved recipes

--- client/src/components/Recipe/SearchResultContainer.js
+++ client/src/components/Recipe/SearchResultContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import SearchForm from "./SearchForm";
 import ResultList from "./ResultList";
+import SavedList from "./SavedList";
 import API from "../../utils/API";
 import axios from 'axios'
 
@@ -16,6 +17,7 @@ class SearchResultContainer extends Component {
   state = {
     search: "",
     results: [],
+    saved: [],
     user: null
   };
 
@@ -65,6 +67,13 @@ class SearchResultContainer extends Component {
       user: this.state.user
     }
     axios.post("/api/recipes", recipeData)
+    this.updateSavedRecipes();
+  }
+
+  updateSavedRecipes = () => {
+    axios.get('./api/user').then(res => {
+      this.setState({saved: res.data.saved})
+    })
   }
 
   render() {
@@ -80,6 +89,8 @@ class SearchResultContainer extends Component {
           <ResultList 
           results={this.state.results}
           handleRecipeSave={this.handleRecipeSave}/>
+          <SavedList
+          saved={this.state.saved}/>
         </Jumbotron>
       </Container>
     );
--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -7,6 +7,18 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+    findByUser: function(req, res) {
+        db.Recipe
+          .find({user: req.params.user})
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
+    findById: function(req, res) {
+        db.Recipe
+          .findById(req.params.id)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
     create: function(req, res) {
         db.Recipe
           .create(req.body)
